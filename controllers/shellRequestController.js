15735cc11d25db32e752402138997a757f68c3b0
javier <richards.javier@gmail.com>
2019-11-13 21:32:28 -0300

    Se agrega operacion in para eliminar por coma en la url con varios ids

--- controllers/shellRequestController.js
+++ controllers/shellRequestController.js
@@ -1,31 +1,43 @@
 const connection = require('../config');
+const Sequelize = require('sequelize');
 const Shell = require('../Models/shell');
+const Op = Sequelize.Op
 
 exports.getShell = function (req, res) {
-    Shell.findAll({
-        where: {
-          id: req.params.shellId
-        }
-      }).then(shell => {
-        res.send(shell)});
+
+  Shell.findAll({
+    where: {
+      id:
+      {
+        [Op.in]: req.params.shellId.split(',')
+      }
+
+    }
+  }).then(shell => {
+    res.send(shell)
+  });
 
 };
 
 
 exports.getShells = function (req, res) {
-    Shell.findAll().then(shell => {
-        res.send(shell);
-      });
+  Shell.findAll().then(shell => {
+    res.send(shell);
+  });
 
 };
 
 
 exports.deleteShell = function (req, res) {
-    Shell.destroy({
-        where: {
-          id: req.params.shellId
-        }
-      });
+  Shell.destroy({
+    where: {
+      id:
+      {
+        [Op.in]: req.params.shellId.split(',')
+      }
+
+    }
+  });
 
 };
 
@@ -33,9 +45,9 @@ exports.deleteShell = function (req, res) {
 // app.post('/eliminado', function (req, res) {
 //     console.log('eliminando', req.body.ids_eliminados  )
 //     connection.query('delete from shell where id in (' + req.body.ids_eliminados+')', function (err, rows, fields) {
-  
+
 //       res.send('eliminado' + req.data);
 //     });
-  
+
 //   });
 
