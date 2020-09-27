ad9849c5911b69974cdbc6dffc41b36adfdd9bbc
jrussellrichards <richards.javier@gmail.com>
2020-09-27 15:37:25 -0300

    se cambia version de node

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 var mysql = require('mysql');
-const port = process.env.PORT ;
+const port = process.env.PORT || 5000;
 app.use(express.json());
 const Routes = require('./routes/shellsRoutes');
 
